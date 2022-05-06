029d3994bab6b39d167b7158c9320f401193a04b
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2022-05-06 09:19:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const cors = require("cors");
 const bodyParser = require('body-parser');
+const cookieParser = require("cookie-parser");
 const morgan = require('morgan')
 const routes = require('./routes');
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
-app.use(express.static("static"));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(morgan('dev'));  // Logger
+app.use(express.static("static"));
 app.set('trust proxy', true);
 app.use((req, res, next) => {  // HTTPS redirection
   if (!req.secure)
