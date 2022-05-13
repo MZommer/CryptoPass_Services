64dfa4c39dbc5fa08e7de7125a3fbff573de85a7
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2022-05-13 11:09:01 +0000

    Updated events

--- routes/v1/events/index.js
+++ routes/v1/events/index.js
@@ -1,24 +1,27 @@
 const events = require("express").Router();
 const DB = require.main.require("./services/db.js");
 
-events.get("/page/:page?", async (req, res, next) => {
-    
+events.get("/search/:page?", async (req, res, next) => {
+    const page = req.params.page || 1;
+    res.json(await DB.searchEvent(req.body, page));
+    next();
 });
 
 events.get("/:id", async (req, res, next) => {
     const event = await DB.getEvent(req.params.id);
     if (event)
-        return res.status(200).json(event);
+        return res.json(event);
     next();
 });
 
-events.post("/create", async (req, res) => {
+events.post("/create", async (req, res, next) => {
     const id = await DB.createEvent(req.body);
-	res.status(200).json({
+	res.json({
         success: true,
         message: "Event created successfully",
 		id,
     });
+    next();
 });
 
 module.exports = events;
--- services/db.js
+++ services/db.js
@@ -27,4 +27,7 @@ module.exports = class {
         event.id = id;
         return event;
     }
+    static async searchEvent(searchParams){
+        // to implement
+    }
 };
