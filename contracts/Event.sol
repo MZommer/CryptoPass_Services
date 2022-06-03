2056bc299aaaa4beddb7736f8ea9483f3df3b242
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2022-06-03 12:02:33 -0300

    Update Event.sol

--- contracts/Event.sol
+++ contracts/Event.sol
@@ -5,16 +5,20 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+
 contract Event is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     
+    mapping(string => mapping(uint256 => address)) private _tickets;
+
     constructor() ERC721("Event", "EVT") {
     }
     
-    function mint(address owner) public returns (uint256){
+    function mint(address owner, string memory ticketType) public returns (uint256) {
         uint256 Id = _tokenIds.current();
         _mint(owner, Id);
+        _tickets[ticketType][Id] = owner;
         _tokenIds.increment();
         return Id;
     }
